499bdb39ce2f02b1c10f30705b7c20a1765161e5
Macbook Air <macbookair@David-Ramirez.netgear.com>
2017-05-15 23:59:28 -0700

    add cllocation manager to get user location

--- stormy/Constants.swift
+++ stormy/Constants.swift
@@ -14,9 +14,9 @@ let LONGITUDE = "&lon="
 let APP_ID = "&appid="
 let API_KEY = "fc8cb60cef288e509031b46d8478e85f"
 
+
+let CURRENT_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&appid=fc8cb60cef288e509031b46d8478e85f"
+
+let FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&cnt=10&mode=json&appid=fc8cb60cef288e509031b46d8478e85f"
+
 typealias DownloadComplete = () -> ()
-
-let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)37\(LONGITUDE)-120\(APP_ID)\(API_KEY)"
-
-let FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=37&lon=-120&cnt=10&mode=json&appid=fc8cb60cef288e509031b46d8478e85f"
-
